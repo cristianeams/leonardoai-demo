75197449d1449e6e6a31666fb019759c0d5d95be
Cristiane Silva <cristiane@neon.cc>
2024-03-04 23:03:00 -0800

    Add personalized favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,9 @@ const inter = Inter({ subsets: ['latin'] });
 export const metadata: Metadata = {
   title: 'Leonardo AI',
   description: 'A coding task for a frontend developer position',
+  icons: {
+    icon: ['/icon.ico'],
+  },
 };
 
 export default function RootLayout({
