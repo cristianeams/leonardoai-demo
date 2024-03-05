1e221c0697760a3337895407ef51e654ec1cdb16
Cristiane Silva <cristiane@neon.cc>
2024-03-04 23:03:22 -0800

    Prevent navigation to private routes logic

--- app/information/page.tsx
+++ app/information/page.tsx
@@ -1,14 +1,44 @@
+'use client';
 import { SimpleGrid, Heading, Box } from '@chakra-ui/react';
 import Header from '../components/Header';
 import { fetchConferences } from '../routes/api/graphql';
 import { Conference } from '@/src/generated/graphql';
 import ConferenceCard from '../components/ConferenceCard';
+import { useEffect, useState } from 'react';
+import { getUserInfo } from '@/utils/userUtils';
+import { useRouter } from 'next/navigation';
+import Loading from '../loading';
 
-export default async function InformationPage() {
-  const { data } = await fetchConferences();
-  console.log(data);
-  const sortedConferences = data?.conferences
-    .slice()
+export default function InformationPage() {
+  const [conferences, setConferences] = useState<Conference[] | null>(null);
+  const router = useRouter();
+  const { userName } = getUserInfo();
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    if (!userName) {
+      setLoading(false);
+      router.push('/welcome');
+    }
+    setLoading(false);
+  }, [userName, router]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const { data } = await fetchConferences();
+      const sortedConferences = data?.conferences
+        .slice()
+        .sort(
+          (a: Conference, b: Conference) =>
+            +new Date(a.startDate) - +new Date(b.startDate)
+        );
+      setConferences(sortedConferences);
+    };
+
+    fetchData();
+  }, []);
+
+  const sortedConferences = conferences
+    ?.slice()
     .sort(
       (a: Conference, b: Conference) =>
         +new Date(a.startDate) - +new Date(b.startDate)
@@ -16,21 +46,27 @@ export default async function InformationPage() {
 
   return (
     <>
-      <Header />
-      <Box px="4">
-        <Heading as="h1" size="xl" py="6" color="white">
-          React Finland Conferences
-        </Heading>
-        <SimpleGrid
-          spacing={4}
-          templateColumns="repeat(auto-fill, minmax(200px, 1fr))"
-          as="ul"
-        >
-          {sortedConferences?.map((conference: Conference) => (
-            <ConferenceCard conference={conference} key={conference.id} />
-          ))}
-        </SimpleGrid>
-      </Box>
+      {loading ? (
+        <Loading />
+      ) : (
+        <>
+          <Header />
+          <Box px="4" pt="16">
+            <Heading as="h1" size="xl" py="6" color="white">
+              React Finland Conferences
+            </Heading>
+            <SimpleGrid
+              spacing={4}
+              templateColumns="repeat(auto-fill, minmax(200px, 1fr))"
+              as="ul"
+            >
+              {sortedConferences?.map((conference: Conference) => (
+                <ConferenceCard conference={conference} key={conference.id} />
+              ))}
+            </SimpleGrid>
+          </Box>
+        </>
+      )}
     </>
   );
 }
---
+++ app/userSettings/page.tsx
@@ -0,0 +1,39 @@
+'use client';
+import { Box, Heading } from '@chakra-ui/react';
+import Header from '../components/Header';
+import EditableUserSettings from '../components/EditableUserSettings';
+import { useRouter } from 'next/navigation';
+import { getUserInfo } from '@/utils/userUtils';
+import Loading from '../loading';
+import { useEffect, useState } from 'react';
+
+export default function SettingsPage() {
+  const router = useRouter();
+  const { userName } = getUserInfo();
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    if (!userName) {
+      setLoading(false);
+      router.push('/welcome');
+    }
+    setLoading(false);
+  }, [userName, router]);
+
+  return (
+    <>
+      {loading ? (
+        <Loading />
+      ) : (
+        <>
+          <Header />
+          <Box px="4" pt="16">
+            <Heading as="h1" size="xl" py="6" color="white">
+              Manage your account
+            </Heading>
+            <EditableUserSettings />
+          </Box>
+        </>
+      )}
+    </>
+  );
+}
