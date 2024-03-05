e0a21a4920d19cc7d44d2c2e1e2217bc0bb8336c
Cristiane Silva <cristiane@neon.cc>
2024-03-04 22:47:00 -0800

    Add EditableUserSettings component to handle updating the users name and job title

---
+++ app/components/EditableUserSettings.tsx
@@ -0,0 +1,63 @@
+'use client';
+import { getUserInfo, setUserInfo } from '@/utils/userUtils';
+import { Button, VStack, Input } from '@chakra-ui/react';
+import { useRouter } from 'next/navigation';
+import { useEffect, useState } from 'react';
+
+export default function EditableUserSettings() {
+  const router = useRouter();
+  const [userName, setUserName] = useState<string>('');
+  const [jobTitle, setJobTitle] = useState<string>('');
+
+  useEffect(() => {
+    const { userName, jobTitle } = getUserInfo();
+    setUserName(userName);
+    setJobTitle(jobTitle);
+  }, []);
+
+  const handleSave = () => {
+    // Update local storage
+    setUserInfo({ userName, jobTitle });
+    setUserName(userName);
+    setJobTitle(jobTitle);
+    router.replace('/information');
+  };
+  return (
+    <>
+      {userName && jobTitle && (
+        <VStack
+          bg="white"
+          borderRadius="lg"
+          p={8}
+          boxShadow="lg"
+          spacing="4"
+          align="flex-start"
+        >
+          <Input
+            placeholder="UserName"
+            size="lg"
+            onChange={(e) => setUserName(e.target.value)}
+            value={userName}
+            maxW="500px"
+          />
+
+          <Input
+            maxW="500px"
+            placeholder="Job Title"
+            size="lg"
+            onChange={(e) => setJobTitle(e.target.value)}
+            value={jobTitle}
+          />
+          <Button
+            colorScheme="red"
+            onClick={handleSave}
+            w={{ base: 'full' }}
+            maxW="500px"
+          >
+            Save
+          </Button>
+        </VStack>
+      )}
+    </>
+  );
+}
